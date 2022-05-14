4d2f67bcbaa956ffc822cc651f3ef11dd42968fe
Gabriela Gurgul <gabrielagurgul@Gabrielas-MacBook-Pro.local>
2022-05-14 16:11:01 +0200

    passed tests for adding too many items

--- test.py
+++ test.py
@@ -13,16 +13,23 @@ def test_add_new_stock_success(no_stock_inventory):
 class InvalidQuantityException(Exception):
         pass
 
+class NoSpaceException(Exception):
+        pass
+
 class Inventory:
     def __init__(self, limit=100):
         self.limit = limit
         self.total_items = 0
         self.stocks = {}
 
-    def add_new_stock(self, name, price, quantity):
+    def add_new_stock(self, name, price, quantity): #?
         if quantity <= 0:
             raise InvalidQuantityException(
-                'Cannot add a quantity of {}. Value must be at least 1'.format(quantity))
+                'Cannot add a quantity of {}. All new stocks must have at least 1 item'.format(quantity))
+        if self.total_items + quantity > self.limit:
+            remaining_space = self.limit - self.total_items
+            raise NoSpaceException(
+                'Cannot add these {} items. Only {} more items can be stored'.format(quantity, remaining_space))
         self.stocks[name] = {
             'price': price,
             'quantity': quantity
@@ -30,19 +37,22 @@ class Inventory:
         self.total_items += quantity
 
     @pytest.mark.parametrize('name,price,quantity,exception', [
-        ('Test Jacket', 10.00, 0, InvalidQuantityException(
-            'Cannot add a quantity of 0. Value must have at least 1 item')),
-        ('Test Jacket', 10.00, 25, NoSpaceException(
-            'Cannot add these 25 items. Only 10 more items can be stored'))
+        ('Test Shoes', 10.00, 0, InvalidQuantityException(
+            'Cannot add a quantity of 0. Value must be at least 1 item'))
+    ])
+
+    @pytest.mark.parametrize('name,price,quantity,exception', [
+        ('Test Shoes', 10.00, 0, InvalidQuantityException(
+            'Cannot add a quantity of 0. Value must be at least 1 item')),
+        ('Test Shoes', 10.00, 25, NoSpaceException(
+            'Cannot add these 25 items. Only 10 items can be added'))
     ])
     def test_add_new_stock_bad_input(name, price, quantity, exception):
         inventory = Inventory(10)
         try:
             inventory.add_new_stock(name, price, quantity)
         except (InvalidQuantityException, NoSpaceException) as inst:
-            # First ensure the exception is of the right type
-            assert isinstance(inst, type(exception))
-            # Ensure that exceptions have the same message
+            assert isinstance(inst, type(exception)) #cehck right side
             assert inst.args == exception.args
         else:
             pytest.fail("Expected error but found none")
