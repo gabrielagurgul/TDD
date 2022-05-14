7aca921f886008bea1c37da8436e78a136c60a9a
Gabriela Gurgul <gabrielagurgul@Gabrielas-MacBook-Pro.local>
2022-05-14 15:28:06 +0200

    Tested negative number exception

--- test.py
+++ test.py
@@ -10,6 +10,9 @@ def test_add_new_stock_success(no_stock_inventory):
     assert no_stock_inventory.stocks['Test Shoes']['price'] == 10.00
     assert no_stock_inventory.stocks['Test Shoes']['quantity'] == 5
 
+class InvalidQuantityException(Exception):
+        pass
+
 class Inventory:
     def __init__(self, limit=100):
         self.limit = limit
@@ -17,10 +20,30 @@ class Inventory:
         self.stocks = {}
 
     def add_new_stock(self, name, price, quantity):
+        if quantity <= 0:
+            raise InvalidQuantityException(
+                'Cannot add a quantity of {}. All new items must be at least 1'.format(quantity))
         self.stocks[name] = {
             'price': price,
             'quantity': quantity
         }
         self.total_items += quantity
 
- 
+    @pytest.mark.parametrize('name,price,quantity,exception', [
+    ('Test Shoes', 10.00, 0, InvalidQuantityException(
+        'Cannot add a quantity of 0. All new stocks must have at least 1 item'))
+    ])
+    def test_add_new_stock_bad_input(name, price, quantity, exception):
+        inventory = Inventory(10)
+        try:
+            inventory.add_new_stock(name, price, quantity)
+        except InvalidQuantityException as inst:
+            assert isinstance(inst, type(exception))
+            assert inst.args == exception.args
+        else:
+            pytest.fail("Expected error but found none")
+
+
+   
+
+    
